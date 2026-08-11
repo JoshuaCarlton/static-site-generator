a3d117e7622a9f58e9684e5d138acf8437118801
JoshuaCarlton <joshuancarlton@gmail.com>
2026-08-11 14:22:15 -0600

    __pycahce__ was being tracked

--- src/textnode.py
+++ src/textnode.py
@@ -2,10 +2,10 @@ from enum import Enum
 
 
 class TextType(Enum):
-    PLAIN_TEXT = "plain"
-    BOLD_TEXT = "bold"
-    ITALIC_TEXT = "italic"
-    CODE_TEXT = "code"
+    PLAIN = "plain"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
     LINK = "link"
     IMAGE = "image"
 
@@ -17,7 +17,7 @@ class TextNode:
         self.url = url
 
     def __eq__(self, value: object, /) -> bool:
-        if isinstance(value, "TextNode"):
+        if isinstance(value, TextNode):
             return bool(self.text == value.text
                 and self.text_type == value.text_type
                 and self.url == value.url)
