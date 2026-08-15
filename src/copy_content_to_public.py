45c32eb5020311710f90e498a852a22db2323515
JoshuaCarlton <joshuancarlton@gmail.com>
2026-08-14 18:36:19 -0600

    recursivly extracts markdown into html from content

---
+++ src/copy_content_to_public.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+from generate_page import generate_page
+
+
+def copy_content_to_public()-> list[str]:
+    abs_content = os.path.abspath("content")
+    abs_public = os.path.abspath("public")
+    if os.path.exists(abs_public):
+        shutil.rmtree(abs_public)
+    os.mkdir(abs_public)
+    return copy_from_to(abs_content, abs_public)
+
+def copy_from_to(from_file: str, to_file: str)-> list[str]:
+    copyed: list[str] = []
+    contents = os.listdir(from_file)
+    abs_template = os.path.abspath("template.html")
+    for content in contents:
+        abs_content = os.path.join(from_file, content)
+        abs_destination = os.path.join(to_file, content)
+        if os.path.isfile(abs_content):
+            if abs_content.endswith(".md"):
+                generate_page(abs_content, abs_template, abs_destination.replace(".md", ".html"))
+            else:
+                shutil.copy(abs_content, abs_destination)
+        else:
+            os.mkdir(abs_destination)
+            copyed.extend(copy_from_to(abs_content, abs_destination))
+        copyed.append(abs_content)
+    return copyed
--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
+from copy_content_to_public import copy_content_to_public
 from copy_static_to_public import copy_static_to_public
-from generate_page import generate_page
 
 
 def main():
     copy_static_to_public()
-    from_path = "content/index.md"
-    dest_path = "public/index.html"
-    template_path = "template.html"
-    generate_page(from_path, template_path, dest_path)
+    copy_content_to_public()
 
 main()
